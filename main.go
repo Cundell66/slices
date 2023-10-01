9f2692562a2f39109af3d5091675dc33855dabc3
Cundell66 <134798569+Cundell66@users.noreply.github.com>
2023-10-02 00:32:03 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	mySlice := []string{"Hi", "There", "How", "Are", "You"}
+
+	fmt.Println(mySlice)
+
+	updateSlice(mySlice)
+
+	fmt.Println(mySlice)
+}
+
+func updateSlice(s []string)  {
+	s[0] = "Bye"
+	
+}
